46f1189659c0c6463af27bdc1f5ec238f9dc3012
uedaeita <eitaro.ueda@gmail.com>
2024-02-01 12:57:25 +0900

    chore: refactor (#4)

--- README.md
+++ README.md
@@ -6,11 +6,10 @@
 
 ## Available Dissectors
 
-- [BPoLTPoE](./bp-over-ltp-over-eth.lua)
+- [DTN BP over LTP over Ethernet](./bp-over-ltp-over-eth.lua)
 
 ## Reference
 
 - [Plugin folders](https://www.wireshark.org/docs/wsug_html_chunked/ChPluginFolders.html)
-  - Windows: `C:\Users\[User Name]\AppData\Roaming\Wireshark\plugins`
-  - Linux  : `/root/.wireshark/plugins`
-  - MacOSX : `~/.config/wireshark/plugins`
+- [Wireshark's Lua API Reference Manual](https://www.wireshark.org/docs/wsdg_html_chunked/wsluarm_modules.html)
+- [LTP Dissector](https://github.com/wireshark/wireshark/blob/master/epan/dissectors/packet-ltp.c)
--- bp-over-ltp-over-eth.lua
+++ bp-over-ltp-over-eth.lua
@@ -1,48 +1,49 @@
--- Wireshark dissector for BP over LTP over Ethernet
-
 set_plugin_info({
     version = "1.0.0",
     author = "uedaeita",
-    repository = "https://github.com/rectoise/wireshark-plugins"
+    repository = "https://github.com/rectoise/wireshark-plugins",
+    description = "Wireshark plugin for DTN BP over LTP over Ethernet Protocol"
 })
 
--- プロトコルの定義
-local bp_over_ltp_over_eth_protocol = Proto("BPoLTPoE", "BP over LTP over Ethernet")
 -- 最小 Ethernet フレーム長
 local min_eth_buffer_len = 64
 -- Ethernet フレームのヘッダ長
 local eth_header_buffer_len = 14
--- 最小 LTP セグメント長
-local min_ltp_buffer_len = min_eth_buffer_len - eth_header_buffer_len
+-- 最小 Ethernet フレームのデータ長
+local min_eth_data_buffer_len = min_eth_buffer_len - eth_header_buffer_len
 
--- プラグインの有効化
-local function enablePlugin()
-    local ether_table = DissectorTable.get("ethertype")
-    for i = 65280, 65536 do -- ff00 ~ ffff
-        ether_table:add(i, bp_over_ltp_over_eth_protocol)
-    end
-end
-
--- Ethernet の最小フレームサイズに満たないデータ長の場合は0埋めしており、Wireshark LTP dissectorが Malformed を返す場合があるため、0埋めされている部分を除去したデータ長のバッファを返す
-local function getPreciseBuffer(buffer)
+--- Tvb (Testy Virtual Buffer) のフォーマット
+local function formatTvb(buffer)
     local buffer_len = buffer:len()
-    if min_ltp_buffer_len < buffer_len then
+
+    if min_eth_data_buffer_len < buffer_len then
         return buffer
     end
 
-    for i = buffer_len - 1, 0, -1 do
+    -- Ethernet の最小フレームサイズに満たないデータ長の場合は0埋めしており、Wireshark LTP dissectorが Malformed を返す場合があるため、0埋めされている部分を除去したデータ長のバッファを返す
+    for i = buffer_len, 0, -1 do
         if buffer(i, 1):uint() ~= 0 then
-            return buffer(0, i + 1):tvb()
+            return buffer(0, i):tvb()
         end
     end
-
     return buffer
 end
 
--- BPoLTPoE dissector
-function bp_over_ltp_over_eth_protocol.dissector(buffer, pinfo, tree)
+--- BP over LTP over Ethernet のプロトコル定義
+local BPoLTPoEProto = Proto("BPoLTPoE", "BP over LTP over Ethernet")
+
+--- BP over LTP over Ethernet のディセクター
+function BPoLTPoEProto.dissector(buffer, pinfo, tree)
     local ltp_dissector = Dissector.get("ltp")
-    ltp_dissector:call(getPreciseBuffer(buffer), pinfo, tree)
+    ltp_dissector:call(formatTvb(buffer), pinfo, tree)
+end
+
+--- プラグインの有効化
+local function enablePlugin()
+    local ether_table = DissectorTable.get("ethertype")
+    for i = 65280, 65536 do -- ff00 ~ ffff
+        ether_table:add(i, BPoLTPoEProto)
+    end
 end
 
 enablePlugin()
